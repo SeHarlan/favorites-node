e7ef7ec5fd2a0cd27da33e7e45a74c2b198ca0c9
Scott Harlan <seharlan@gmail.com>
2020-02-27 16:12:09 -0800

    routes

---
+++ app.js
@@ -0,0 +1,127 @@
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const client = require('./lib/client');
+// Initiate database connection
+client.connect();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // server files from /public folder
+app.use(express.json()); // enable reading incoming json data
+app.use(express.urlencoded({ extended: true }));
+
+// API Routes
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, hash 
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+            INSERT into users (email, hash)
+            VALUES ($1, $2)
+            RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
+// before ensure auth, but after other middleware:
+app.use('/api/auth', authRoutes);
+
+// for every route, on every request, make sure there is a token
+const ensureAuth = require('./lib/auth/ensure-auth');
+
+app.use('/api', ensureAuth);
+
+//connect to swapi
+const request = require('superagent');
+
+// *** FAVVVVVV ***
+app.get('/api/characters', async(req, res) => {
+    try {
+        const data = await request.get(`https://swapi.co/api/people/?search=${req.query.search}`);
+        res.json(data.body);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.get('/api/me/favorites', async(req, res) => {
+    try {
+        const result = await client.query(`
+            SELECT * FROM favorites
+            WHERE user_id = $1;
+        `, [req.userId]);
+
+        res.json(result.rows);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.post('/api/me/favorites', async(req, res) => {
+    try {
+        const result = await client.query(`
+            INSERT INTO favorites
+            (name, birth_year, homeworld_name, homeworld_climate, homeworld_population, user_id)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `, [req.body.name, req.body.birth_year, req.body.homeworld_name, req.body.homeworld_climate, req.body.homeworld_population, req.userId]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.delete('/api/me/favorites/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const result = await client.query(`
+            DELETE FROM favorites
+            WHERE id = $1
+            AND user_id = $2;
+        `, [id, req.userId]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server running on PORT', process.env.PORT);
+});
---
+++ data/create-tables.js
@@ -0,0 +1,41 @@
+const client = require('../lib/client');
+
+// async/await needs to run in a function
+run();
+
+async function run() {
+
+    try {
+        // initiate connecting to db
+        await client.connect();
+
+        // run a query to create tables
+        await client.query(`
+                CREATE TABLE users (
+                    id SERIAL PRIMARY KEY,
+                    email VARCHAR(256) NOT NULL,
+                    hash VARCHAR(512) NOT NULL
+                );           
+                CREATE TABLE favorites (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    name VARCHAR(512) NOT NULL,
+                    birth_year VARCHAR(512) NOT NULL,
+                    homeworld_name VARCHAR(512) NOT NULL,
+                    homeworld_climate VARCHAR(512) NOT NULL,
+                    homeworld_population VARCHAR(512) NOT NULL,
+                    user_id INTEGER NOT NULL REFERENCES users(id)
+            );
+        `);
+
+        console.log('create tables complete');
+    }
+    catch (err) {
+        // problem? let's see the error...
+        console.log(err);
+    }
+    finally {
+        // success or failure, need to close the db connection
+        client.end();
+    }
+
+}
